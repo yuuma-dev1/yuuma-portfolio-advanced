7210d9fbe4183da113b2198c609739edc323e783
yuuma-dev1 <yuuma.dev1@gmail.com>
2026-03-24 16:06:41 +0900

    Create lp.js

---
+++ lp.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+reveals.forEach((element) => {
+  observer.observe(element);
+});
+
